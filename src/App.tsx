3b70959977f30f0fce712fce507542fffc7ac3cd
Dad <Dad@mac.home>
2025-06-24 23:35:15 +0100

    replaced app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { useAuthenticator } from '@aws-amplify/ui-react';
-import { generateClient } from "aws-amplify/data";
 import React from 'react';
 import { uploadData } from 'aws-amplify/storage';
 
-const client = generateClient<Schema>();
-
 function App() {
-    const { user, signOut } = useAuthenticator();
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
   const [file, setFile] = React.useState();
-}
 
-    const handleChange = (event) => {
+  const handleChange = (event) => {
     setFile(event.target.files?.[0]);
   };
 
-  const handleClick = () => 
+  const handleClick = () => {
     if (!file) {
       return;
     }
@@ -25,45 +16,12 @@ function App() {
       path: `picture-submissions/${file.name}`,
       data: file,
     });
+  };
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-  
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
   return (
-    <main>
-            <h1>{user?.signInDetails?.loginId}'s ScoutSnap</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li 
-                    onClick={() => deleteTodo(todo.id)}
-          key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-      <div>
+    <div>
       <input type="file" onChange={handleChange} />
       <button onClick={handleClick}>Upload</button>
     </div>
-            <button onClick={signOut}>Sign out</button>
-    </main>
   );
 }
-
-export default App;
