27bc926b3c4484ea9c7dbfe955f5a7c41a8803da
Dad <Dad@mac.home>
2025-06-24 23:52:58 +0100

    added upload button again

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,33 @@ import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { useAuthenticator } from '@aws-amplify/ui-react';
 import { generateClient } from "aws-amplify/data";
+import { uploadData } from 'aws-amplify/storage';
 
 const client = generateClient<Schema>();
 
 function App() {
     const { user, signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [file, setFile] = React.useState();
+  const handleChange = (event) => {
+    setFile(event.target.files?.[0]);
+  };
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
   }, []);
+
+   const handleClick = () => {
+    if (!file) {
+      return;
+    }
+    uploadData({
+      path: `picture-submissions/${file.name}`,
+      data: file,
+    });
+  };
 
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
@@ -26,6 +41,8 @@ function App() {
     <main>
             <h1>{user?.signInDetails?.loginId}'s ScoutSnap</h1>
       <button onClick={createTodo}>+ new</button>
+      <input type="file" onChange={handleChange} />
+      <button onClick={handleClick}>Upload</button>
       <ul>
         {todos.map((todo) => (
           <li 
