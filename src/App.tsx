a92ef64c49dc9d62f68e6579708dd4d58dbccfa5
Dad <Dad@mac.home>
2025-06-24 23:39:56 +0100

    restored app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,48 @@
-import React from 'react';
-import { uploadData } from 'aws-amplify/storage';
+import { useEffect, useState } from "react";
+import type { Schema } from "../amplify/data/resource";
+import { useAuthenticator } from '@aws-amplify/ui-react';
+import { generateClient } from "aws-amplify/data";
+
+const client = generateClient<Schema>();
 
 function App() {
-  const [file, setFile] = React.useState();
+    const { user, signOut } = useAuthenticator();
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  const handleChange = (event) => {
-    setFile(event.target.files?.[0]);
-  };
-
-  const handleClick = () => {
-    if (!file) {
-      return;
-    }
-    uploadData({
-      path: `picture-submissions/${file.name}`,
-      data: file,
+  useEffect(() => {
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
     });
-  };
+  }, []);
 
+  function createTodo() {
+    client.models.Todo.create({ content: window.prompt("Todo content") });
+  }
+  
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
   return (
-    <div>
-      <input type="file" onChange={handleChange} />
-      <button onClick={handleClick}>Upload</button>
-    </div>
+    <main>
+            <h1>{user?.signInDetails?.loginId}'s ScoutSnap</h1>
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map((todo) => (
+          <li 
+                    onClick={() => deleteTodo(todo.id)}
+          key={todo.id}>{todo.content}</li>
+        ))}
+      </ul>
+      <div>
+        🥳 App successfully hosted. Try creating a new todo.
+        <br />
+        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+          Review next step of this tutorial.
+        </a>
+      </div>
+            <button onClick={signOut}>Sign out</button>
+    </main>
   );
 }
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
+
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
